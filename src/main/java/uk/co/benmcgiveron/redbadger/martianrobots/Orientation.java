9f3f3e6a2325a86964e44011628623d89f422611
Ben <ben@bpmcgiveron.co.uk>
2017-09-03 10:28:09 +0100

    Added to and from string to Orientation

--- src/main/java/uk/co/benmcgiveron/redbadger/martianrobots/Orientation.java
+++ src/main/java/uk/co/benmcgiveron/redbadger/martianrobots/Orientation.java
@@ -1,8 +1,26 @@
 package uk.co.benmcgiveron.redbadger.martianrobots;
 
 public enum Orientation {
-	NORTH,
-	SOUTH,
-	EAST,
-	WEST
+	NORTH("N"),
+	SOUTH("S"),
+	EAST("E"),
+	WEST("W");
+	
+	private String stringRepresentation;
+	
+	private Orientation(String stringRepresentation) {
+		this.stringRepresentation = stringRepresentation;
+	}
+	
+	@Override
+	public String toString() {
+		return stringRepresentation;
+	}
+	
+	public static Orientation fromString(String orientation) {
+		for(Orientation o : Orientation.values()) {
+			if(o.toString().equals(orientation)) return o;
+		}
+		return null;
+	}
 }
